8f510fb5a835dc28ae42c89c2a05f80bf833b9de
Ebtesham-21 <ebteshamhossain98@gmail.com>
2025-02-16 18:52:06 +0600

    02 svg logo

---
+++ lib/constants.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "99 BOOKS"
+export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || "BOOKS FOR EVERYONE"
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || "99 BOOKS"
