3997ecd433215896fd911faf8da5a6f6b501a3f3
Ebtesham-21 <ebteshamhossain98@gmail.com>
2025-02-17 16:31:19 +0600

    04 data for search menu

---
+++ lib/data.ts
@@ -0,0 +1,45 @@
+
+
+const data = {
+    headerMenus: [
+        {
+        name: "Today's Deal",
+        href="/search?tag=todays-deal",
+        },
+        {
+        name: "New Arrivals",
+        href="/search?tag=new-arrival",
+        },
+        {
+        name: "Featured Products",
+        href="/search?tag=featured"
+        },
+        {
+        name: "Best Sellers",
+        href="/search?tag=Best Sellers"
+        },
+        {
+        name: "Best Sellers",
+        href="/search?tag=Best Sellers"
+        },
+        {
+        name: "Browsing History",
+        href="/#browsing-history",
+        },
+        {
+        name: "Customer Service",
+        href="/page/customer-service",
+        },
+        {
+        name: "About Us",
+        href="/page/about-us",
+        },
+        {
+        name: "Help",
+        href="/page/help",
+        },
+
+    ],
+}
+
+export default data
