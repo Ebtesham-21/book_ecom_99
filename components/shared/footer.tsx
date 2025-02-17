36a76dcf9f5983086883ca69dcd44b591d0d708f
Ebtesham-21 <ebteshamhossain98@gmail.com>
2025-02-17 16:44:15 +0600

    05 footer

---
+++ components/shared/footer.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { ChevronUp } from "lucide-react"
+import { Button } from "../ui/button"
+import Link from "next/link"
+import { APP_NAME } from "@/lib/constants"
+
+export default function Footer() {
+
+    return (
+        
+    )
+}
