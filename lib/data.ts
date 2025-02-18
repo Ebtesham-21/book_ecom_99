e8a24432c8203cea6036b7967d2af58087d9ee4f
Ebtesham-21 <ebteshamhossain98@gmail.com>
2025-02-18 18:00:27 +0600

    data base reform

--- lib/data.ts
+++ lib/data.ts
@@ -1,6 +1,69 @@
+import {Data, IProductInput} from '@/types'
+import { toSlug } from './utils'
+
+const products: IProductInput[] = [
+    {
+        name: 'Lily Day',
+        slug: toSlug('Lily Day'),
+        category: 'Kids Books',
+        images: ['/images/book1.png'],
+        tags: ['new-arrival'],
+        isPublished: true,
+        price: 200,
+        listPrice: 0,
+        brand: '99Books',
+        avgRating: 4.71,
+        numReviews: 7,
+        ratingDistribution: [
+            {rating: 1, count: 0},
+            {rating: 2, count: 0},
+            {rating: 3, count: 0},
+            {rating: 4, count: 2},
+            {rating: 5, count: 5},
+        ],
+        numSales: 9,
+        countInStock: 11,
+        description: "Kids Shop",
+        sizes: ['S', 'M'],
+        colors: ['Green', 'Red', 'Black'],
+        reviews: [],
 
 
-const data = {
+    },
+    {
+        name: 'Lily Day',
+        slug: toSlug('Lily Day'),
+        category: 'Kids Books',
+        images: ['/images/book2.png'],
+        tags: ['new-arrival'],
+        isPublished: true,
+        price: 200,
+        listPrice: 0,
+        brand: '99Books',
+        avgRating: 4.71,
+        numReviews: 7,
+        ratingDistribution: [
+            {rating: 1, count: 0},
+            {rating: 2, count: 0},
+            {rating: 3, count: 0},
+            {rating: 4, count: 2},
+            {rating: 5, count: 5},
+        ],
+        numSales: 9,
+        countInStock: 11,
+        description: "Kids Shop",
+        sizes: ['S', 'M'],
+        colors: ['Green', 'Red', 'Black'],
+        reviews: [],
+
+
+    },
+]
+
+
+
+const data : Data = {
+    products,
     headerMenus: [
         {
         name: "Today's Deal",
