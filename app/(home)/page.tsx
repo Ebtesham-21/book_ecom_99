03b9b167fa338cd30db0b55cef5b294c477f0040
Ebtesham-21 <ebteshamhossain98@gmail.com>
2025-02-20 15:23:20 +0600

    cards issue

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -69,6 +69,8 @@ export default async function Page() {
 
 
   const todaysDeals = await getProductsByTag({tag: 'todays-deal'})
+  const bestSellingProducts = await getProductsByTag({tag: 'best-seller'})
+
   return(
   <>
     <HomeCarousel items={data.carousels} />
@@ -79,6 +81,17 @@ export default async function Page() {
           <ProductSlider title={"Today's Deals"} products={todaysDeals} />
         </CardContent>
 
+      </Card>
+
+      <Card className="w-full rounded-none">
+        <CardContent className="p-4 items-center gap-3" >
+          <ProductSlider 
+            title="Best Selling Products"
+            products={bestSellingProducts}
+            hideDetails
+          />
+
+        </CardContent>
       </Card>
 
     </div>
